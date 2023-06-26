f42694480934513480c9317181397fe59964ab55
Nikita <Mandalika>
2023-06-26 10:21:33 -0400

    Initial Commit

---
+++ set-up-Github/set_up_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  set_up_GithubApp.swift
+//  set-up-Github
+//
+//  Created by Nikita Mandalika on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct set_up_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
